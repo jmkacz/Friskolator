bc4e25d329893984f34ec8a0d3d4962ad5042f0c
Jon Kaczynski <jmkacz@gmail.com>
2012-07-13 13:07:49 -0400

    added metadata to the template; made event_data_dir more robust

--- stream.py
+++ stream.py
@@ -1,16 +1,23 @@
+import os
+
 #defines info for collecting data from flickr
 user_id = "81953314@N02"
 event_tag = "prehope_test"
 
 #data directory
-event_data_dir = '../Friskolator_data/' + event_tag
-
+project_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+event_data_dir = os.path.join(
+    project_path,
+    "Friskolator_data",
+    event_tag
+)
 
 #template for a frisk event 
 # Must match data outline in Friskolator_data/README.md 
 # for this to work
 template_frisk = {
-	"version":"0.5", # cur version of this template
-	"event": event_tag,
-	"constented":True,
+    "version":"0.5", # cur version of this template
+    "event": event_tag,
+    "constented":True,
+    "metadata": {},
 }
